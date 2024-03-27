22832b4bcdfbd14eeb3c5237249ee10465edaab3
cramirez16 <cramirez16a@gmail.com>
2024-03-27 17:35:39 +0000

    test real component header

--- src/app/todos/components/header/header.component.spec.ts
+++ src/app/todos/components/header/header.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { TodosService } from '../../services/todos.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let todosService: TodosService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
+      imports: [HeaderComponent, HttpClientTestingModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    todosService = TestBed.inject(TodosService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a Todo', () => {
+    jest.spyOn(todosService, 'addTodo').mockImplementation(() => {});
+    const input = fixture.debugElement.query(
+      By.css('[data-testid="newTodoInput"]')
+    );
+    input.nativeElement.value = 'foo';
+    input.nativeElement.dispatchEvent(
+      new KeyboardEvent('keyup', { key: 'Enter' })
+    );
+    expect(todosService.addTodo).toHaveBeenCalledWith('foo');
+    expect(component.text).toEqual('');
   });
 });
